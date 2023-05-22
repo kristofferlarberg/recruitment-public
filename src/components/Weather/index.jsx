a5fba8776dccb4c745d41505c1d5af8d8733cde0
kristofferlarberg <kristoffer.larberg@icloud.com>
2023-05-22 19:22:27 +0200

    feat: fetch from API

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-import styles from "./Weather.module.css";
+import React, { useEffect } from 'react';
+import styles from './Weather.module.css';
+
+// todo
+// 1. setup fetch from API
+// 2. save data to state and render in UI
+// 3. setup form
+// 4. make fetch from API with form input
+// 5. add icon and styling
 
 const Weather = () => {
+  const token = import.meta.env.VITE_API_TOKEN;
+
+  useEffect(() => {
+    const weatherData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${token}&units=metric`
+        );
+        const data = await response.json();
+        console.log(data.weather[0].main);
+      } catch {
+        console.log('Data not found');
+      }
+    };
+    weatherData();
+  });
+
   return (
     <div>
       <h2>How's the weather out there?</h2>
