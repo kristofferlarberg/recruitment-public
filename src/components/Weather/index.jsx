90ba85ee708e2227ee3b9824556493e7cca0ed44
kristofferlarberg <kristoffer.larberg@icloud.com>
2023-05-22 21:16:04 +0200

    feat: setup form and connect to api call

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import styles from './Weather.module.css';
 
 // todo
@@ -6,42 +7,59 @@ import styles from './Weather.module.css';
 // 2. save data to state and render in UI
 // 3. setup form
 // 4. make fetch from API with form input
-// 5. add icon and styling
+// 5. validate input
+// 6. add contitional copy based on weather
+// 7. add icon and styling
 
 const Weather = () => {
   const [weather, setWeather] = useState('');
+  const [city, setCity] = useState('');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const token = import.meta.env.VITE_API_TOKEN;
 
   useEffect(() => {
     const weatherData = async () => {
-      try {
-        const response = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${token}&units=metric`
-        );
-        const data = await response.json();
-        setWeather(data.weather[0].main);
-      } catch {
-        console.log('Data not found');
+      if (city) {
+        try {
+          const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${token}&units=metric`
+          );
+          const data = await response.json();
+          setWeather(data.weather[0].main);
+        } catch {
+          console.log('Data not found');
+        }
       }
     };
     weatherData();
-  }, []);
+  }, [city]);
+
+  const onSubmit = (input) => setCity(input.city);
+
+  console.log('errors', errors);
 
   return (
     <div>
       <h2>How's the weather out there?</h2>
-      <form>
-        <p>The weather is {weather.toLowerCase()}</p>
+      {weather && (
+        <p>
+          The weather in {city[0].toUpperCase() + city.slice(1)} is{' '}
+          {weather.toLowerCase()}
+        </p>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
+          className={styles.textInput}
           type="text"
           placeholder="Enter City"
-          maxLength={50}
-          className={styles.textInput}
+          {...register('city', { required: true, maxLength: 50 })}
         />
-        <button className={styles.Button} type="submit">
-          Get Weather
-        </button>
+        <input className={styles.Button} type="submit" value="Get Weather" />
       </form>
     </div>
   );
